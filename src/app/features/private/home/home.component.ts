170ad6df563b1605f34b5fdc29bcfdc8315695b1
Diego Gettatelli <dgettatelli@gmail.com>
2024-10-03 22:38:46 +0200

    feat: add dashboard home components nad positioning

--- src/app/features/private/home/home.component.ts
+++ src/app/features/private/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import {CharacterCardComponent} from "../character-card/character-card.component";
+import {HeaderComponent} from "../../../core/header/header.component";
+import {FooterComponent} from "../../../core/footer/footer.component";
+import {CustomCardComponent} from "../../../core/custom-card/custom-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CharacterCardComponent
+    CharacterCardComponent,
+    HeaderComponent,
+    FooterComponent,
+    CustomCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
