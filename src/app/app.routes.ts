8b61440a44163056a294676133c2b1ed6b4b8288
Diego Gettatelli <dgettatelli@gmail.com>
2024-10-08 19:16:57 +0200

    feat: add character list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./features/public/login/login.component";
 import {HomeComponent} from "./features/private/home/home.component";
 import {DefaultLayoutComponent} from "./core/default-layout/default-layout.component";
+import {CharacterListComponent} from "./features/private/character-list/character-list.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path:'', component: DefaultLayoutComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'characters', component: CharacterListComponent}
     ]},
 
   {path: '', redirectTo: '/login', pathMatch: 'full'}
