5e3c4c80d489427540ed1b9109c52018dedb247c
Diego Gettatelli <dgettatelli@gmail.com>
2024-10-15 15:03:31 +0000

    feat: wip custom-card style

--- src/app/features/private/custom-card-section/custom-card-section.component.ts
+++ src/app/features/private/custom-card-section/custom-card-section.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { CustomCardComponent } from '../../../core/custom-card/custom-card.component';
 
 @Component({
   selector: 'app-custom-card-section',
   standalone: true,
-  imports: [],
+  imports: [CustomCardComponent],
   templateUrl: './custom-card-section.component.html',
-  styleUrl: './custom-card-section.component.css'
+  styleUrl: './custom-card-section.component.css',
 })
-export class CustomCardSectionComponent {
-
-}
+export class CustomCardSectionComponent {}
