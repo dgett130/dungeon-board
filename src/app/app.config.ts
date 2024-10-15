90b8ec69aaa610e09e122ce124bcc3198cc0aea4
Diego Gettatelli <dgettatelli@gmail.com>
2024-10-15 14:09:17 +0000

    feat: add Google oauth2 login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,30 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  GoogleLoginProvider,
+  SocialAuthServiceConfig,
+} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            // Inserisci al posto di 'clientID' il tuo id personale
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('clientId'),
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        },
+      } as SocialAuthServiceConfig,
+    },
+  ],
 };
--- src/app/features/public/login/login.component.ts
+++ src/app/features/public/login/login.component.ts
@@ -1,26 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-import {FormsModule} from "@angular/forms";
-import {Router} from "@angular/router";
-import {routes} from "../../../app.routes";
+import { Component, OnInit, inject } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  GoogleSigninButtonModule,
+  SocialAuthService,
+  SocialLoginModule,
+  SocialUser,
+} from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    FormsModule
+    FormsModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
-  constructor(private  router: Router) { }
+  user: SocialUser = new SocialUser();
+  loggedIn: boolean = false;
 
-  ngOnInit() { }
+  constructor(private router: Router, private authService: SocialAuthService) {}
 
-  onSubmit() {
-    this.router.navigate(['/home']).then(r => console.log(r));
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      console.log(user);
+      this.user = user;
+      this.loggedIn = user != null;
+    });
   }
 
+  onSubmit() {
+    this.router.navigate(['/home']).then((r) => console.log(r));
+  }
+
+  onGoogleLogin() {}
 }
